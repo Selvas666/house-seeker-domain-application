77c3304d798f25ddf1f4ee231b40ab3af14bb072
Selvas666 <tomgad@gmail.com>
2022-09-24 17:00:12 +0200

    added mapper interface

---
+++ house-seeker-domain/src/main/java/pl/kala/houseseekerdomain/domain/mapping/Mapper.java
@@ -0,0 +1,28 @@
+package pl.kala.houseseekerdomain.domain.mapping;
+
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+
+public interface Mapper <S, T> {
+
+    T convert (S var1);
+
+    default Try<T> tryConvert(S source){
+        return Try.of(()->{
+            return this.convert(source);
+        });
+    }
+
+    default Option<T> optionConvert(S source){
+        return Try.of(() -> {
+            return this.convert(source);
+        }).toOption();
+    }
+
+    default Either<RuntimeException, T> eitherConvert(S source){
+        return Try.of(() -> {
+            return this.convert(source);
+        }).toEither().mapLeft(RuntimeException::new);
+    }
+}
