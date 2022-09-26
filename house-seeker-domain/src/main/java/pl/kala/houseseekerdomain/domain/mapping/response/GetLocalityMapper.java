3a1122bb01c7bd5cf3e1fe7da298326c7aabc3e2
Selvas666 <tomgad@gmail.com>
2022-09-26 15:59:54 +0200

    added GetLocalityMapperSpec with standard tests and fixed GetLocalityMapper.java

--- house-seeker-domain/src/main/java/pl/kala/houseseekerdomain/domain/mapping/response/GetLocalityMapper.java
+++ house-seeker-domain/src/main/java/pl/kala/houseseekerdomain/domain/mapping/response/GetLocalityMapper.java
@@ -2,6 +2,7 @@ package pl.kala.houseseekerdomain.domain.mapping.response;
 
 import pl.kala.houseseekerdomain.database.model.document.locality.Locality;
 import pl.kala.houseseekerdomain.domain.mapping.Mapper;
+import pl.kala.houseseekerdomain.domain.mapping.utils.MapperUtils;
 import pl.kala.houseseekerdomain.domain.model.response.dto.GetLocalityDto;
 
 public class GetLocalityMapper implements Mapper<Locality, GetLocalityDto> {
@@ -9,7 +10,7 @@ public class GetLocalityMapper implements Mapper<Locality, GetLocalityDto> {
     public GetLocalityDto convert(Locality locality) {
         return GetLocalityDto.builder()
                 .name(locality.getName())
-                .voivodeship(locality.getVoivodship().getLabel())
+                .voivodeship(locality.getVoivodship() == null ? MapperUtils.emptyString() : locality.getVoivodship().getLabel())
                 .distanceFromHome(locality.getDistanceFromHome())
                 .distanceFromRailStation(locality.getDistanceFromRailStation())
                 .build();
---
+++ house-seeker-domain/src/test/groovy/pl/kala/houseseekerdomain/domain/mapping/response/GetLocalityMapperSpec.groovy
@@ -0,0 +1,37 @@
+package pl.kala.houseseekerdomain.domain.mapping.response
+
+import pl.kala.houseseekerdomain.UnitSpecificationConfiguration
+import pl.kala.houseseekerdomain.database.model.document.locality.Locality
+import pl.kala.houseseekerdomain.domain.model.response.dto.GetLocalityDto
+
+class GetLocalityMapperSpec extends UnitSpecificationConfiguration {
+    GetLocalityMapper mapper = new GetLocalityMapper()
+
+    def "GetLocalityMapper mapping a minimal Locality object with only id and name fields filled in"() {
+        given: "A Locality object with only id and name fields filled in"
+        Locality locality = Locality.builder()
+                .id("1")
+                .name("Wrocław")
+                .build()
+        when: "The mapper maps the object"
+        GetLocalityDto result = mapper.convert(locality)
+        then: "We get a GetLocalityDto object with only name field filled in"
+        result.getName() == locality.getName()
+        result.getVoivodeship() == ""
+        result.getDistanceFromHome() == null
+        result.getDistanceFromRailStation() == null
+    }
+
+    def "GetLocalityMapper mapping a  Locality object with all the fields filled in"() {
+        given: "A Locality object with all the fields filled in"
+        Locality locality = Locality.random()
+        when: "The mapper maps the object"
+        GetLocalityDto result = mapper.convert(locality)
+        then: "We get a GetLocalityDto object with all the fields properly filled in"
+        result.getName() == locality.getName()
+        result.getVoivodeship() == locality.getVoivodship().getLabel()
+        result.getDistanceFromHome() == locality.getDistanceFromHome()
+        result.getDistanceFromRailStation() == locality.getDistanceFromRailStation()
+    }
+
+}
