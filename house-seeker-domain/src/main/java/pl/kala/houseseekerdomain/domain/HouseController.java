652bc7af8fb8186fd4aadfbdd5b00ee5163dc40f
Selvas666 <tomgad@gmail.com>
2022-09-27 00:40:26 +0200

    added it tests and get /all

--- house-seeker-domain/src/main/java/pl/kala/houseseekerdomain/domain/HouseController.java
+++ house-seeker-domain/src/main/java/pl/kala/houseseekerdomain/domain/HouseController.java
@@ -3,14 +3,14 @@ package pl.kala.houseseekerdomain.domain;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.kala.houseseekerdomain.domain.logic.house.HouseApi;
 import pl.kala.houseseekerdomain.domain.model.request.CreateHouseRequest;
+import pl.kala.houseseekerdomain.domain.model.response.GetAllHousesResponse;
 
 @Slf4j
-@RestController("/api/house")
+@RestController
+@RequestMapping("/api/house")
 @RequiredArgsConstructor
 public class HouseController {
     private final HouseApi houseApi;
@@ -19,8 +19,15 @@ public class HouseController {
     ResponseEntity<Void> saveHouse(@RequestBody CreateHouseRequest request) {
         houseApi.saveHouse(request)
                 .onFailure(ex -> log.error("Failed to save house - {}", ex.getMessage()));
-
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/all")
+    ResponseEntity<GetAllHousesResponse> getAllHouses() {
+        GetAllHousesResponse response =  houseApi.getAllHouses()
+                .onFailure(ex -> log.error("Failed to get all houses - {}", ex.getMessage()))
+                .get();
+        return ResponseEntity.ok(response);
     }
 
 }
--- house-seeker-domain/src/test/groovy/pl/kala/houseseekerdomain/domain/HouseControllerIT.groovy
+++ house-seeker-domain/src/test/groovy/pl/kala/houseseekerdomain/domain/HouseControllerIT.groovy
@@ -1,17 +1,33 @@
 package pl.kala.houseseekerdomain.domain
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import io.restassured.specification.RequestSpecification
+import lombok.extern.slf4j.Slf4j
+import org.springframework.boot.test.web.server.LocalServerPort
 import pl.kala.houseseekerdomain.IntegrationSpecificationConfiguration
 
+@Slf4j
 class HouseControllerIT extends IntegrationSpecificationConfiguration{
+    @LocalServerPort
+    int port
+    def"I cant save a house" () {
+        given: "a minimum possible body for saving a house"
+        log.info("The port of silver is:${port.toString()}")
+        RestAssured.port = port
+        RequestSpecification request = RestAssured.given()
+        request.basePath("/api/house/save")
+        File body = new File("src/test/resources/json/request/saveHouseMin.json")
 
-    @Autowired
-    ApplicationContext applicationContext;
+        request.contentType(ContentType.JSON)
+        request.body(body)
 
-    def"context loads" () {
-        when:""
-        then:""
-        printf ("%s",applicationContext.getApplicationName())
+
+        when:"we send that"
+        def result = request.post()
+
+        then:"it should get positive response xd"
+        result.statusCode() == 200
+
     }
 }
