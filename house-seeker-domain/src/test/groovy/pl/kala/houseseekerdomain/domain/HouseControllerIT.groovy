391f4e494efcc22705c0555727e83d0d5b12c971
Selvas666 <tomgad@gmail.com>
2022-09-28 15:14:21 +0200

    reformat

--- house-seeker-domain/src/test/groovy/pl/kala/houseseekerdomain/domain/HouseControllerIT.groovy
+++ house-seeker-domain/src/test/groovy/pl/kala/houseseekerdomain/domain/HouseControllerIT.groovy
@@ -4,7 +4,6 @@ import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import io.restassured.specification.RequestSpecification
 import lombok.extern.slf4j.Slf4j
-import org.hamcrest.Matchers
 import org.springframework.http.HttpStatus
 import pl.kala.houseseekerdomain.IntegrationSpecificationConfiguration
 
